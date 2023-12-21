1f8a8f76404972428a8959e10c4db04b1fdee773
Ale <darmdz23@gmail.com>
2023-12-21 12:45:14 -0400

    prueba1

--- enemy1.gd
+++ enemy1.gd
@@ -10,9 +10,7 @@ class_name enemy
 @export_enum("Slow:30", "Average:60", "Very Fast:200") var character_speed: int
 @export_dir var image: String
 
-@export_exp_easing var transition_speed
-@export_exp_easing("attenuation") var fading_attenuation
-@export_exp_easing("positive_only") var effect_power
+
 ######
 func _ready():
 	print(dye_color)
